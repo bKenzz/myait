d20dc23de0dedf5e2e22754e1b350dd61fb989a5
bKenzz <kenzhebaevbektur5@gmail.com>
2024-04-14 12:40:02 +0600

    [#2] My commit message here.

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:myait/models/myuser.dart';
+import 'package:myait/screens/authenticate/signin.dart';
 import 'package:myait/screens/wrapper.dart';
 import 'package:myait/services/auth.dart';
 import 'firebase_options.dart';
@@ -26,7 +27,7 @@ class MyApp extends StatelessWidget {
       value: AuthService().user,
       initialData: null,
       child: MaterialApp(
-        home: Wrapper(),
+        home: Sign_in(),
       ),
     );
   }
---
+++ lib/screens/authenticate/signin.dart
@@ -0,0 +1,17 @@
+// Hex Color Codes
+// #18363E rgb(24, 54, 62)
+// #5F97AA rgb(95, 151, 170)
+// #2D5F6E rgb(45, 95, 110)
+// #3E88A5 rgb(62, 136, 165)
+// #93C4D1 rgb(147, 196, 209)
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class Sign_in extends StatelessWidget {
+  const Sign_in({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
